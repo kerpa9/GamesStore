aad6a5c08669dc8a6209e781003337188209332d
Kevin Reyes <kerpa9@gmail.com>
2024-09-14 18:51:44 -0500

    Handle errors and extend a class

---
+++ src/domain/errors/custom.erros.ts
@@ -0,0 +1,20 @@
+export class CatchError extends Error {
+  constructor(public readonly message: string, public readonly status: number) {
+    super(message);
+  }
+  static badRequest(message: string) {
+    return new CatchError(message, 400);
+  }
+
+  static unAuthorized(message: string) {
+    return new CatchError(message, 401);
+  }
+
+  static notFound(message: string) {
+    return new CatchError(message, 404);
+  }
+
+  static internalServer(message: string) {
+    return new CatchError(message, 500);
+  }
+}
