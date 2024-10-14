327285481c3e471e3a47d61ca4cee60a863fb3b7
Kevin Reyes <kerpa9@gmail.com>
2024-10-14 12:46:13 -0500

    Initial firebase

---
+++ src/config/firebaseAdapter.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDBEqpzIcrRwcIJcYtWDyxP4WBKagFqlOA",
+  authDomain: "gamestore-fe181.firebaseapp.com",
+  projectId: "gamestore-fe181",
+  storageBucket: "gamestore-fe181.appspot.com",
+  messagingSenderId: "442450072762",
+  appId: "1:442450072762:web:7df892479700d791e59e83",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/config/uploadFilesAdapter.ts
+++ src/config/uploadFilesAdapter.ts
@@ -4,6 +4,19 @@ const storage = multer.memoryStorage();
 
 const upload = multer({ storage });
 
+//Configure type date send multer
+// const upload = multer({
+//   storage,
+//   fileFilter: (request, file, cb) => {
+//     if (file.mimetype == "image/jpg") {
+//       cb(null, true);
+//     } else {
+//       cb(null, false);
+//       return cb(new Error("Only .png format allowed!"));
+//     }
+//   },
+// });
+
 export const uploadSingle = (filename: string) => upload.single(filename);
 
 export const uploadArray = (filename: string, maxData: number) =>
